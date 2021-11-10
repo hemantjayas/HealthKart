cc541f25edbc1e0fc9a11b492cf02e29e570f0ce
arpit1024 <91615308+arpit1024@users.noreply.github.com>
2021-11-10 12:37:55 +0530

    Add files via upload

---
+++ scripts/menWomen.js
@@ -0,0 +1,420 @@
+let productsMen = [
+    {
+        imgflash: 'https://img10.hkrtcdn.com/react/static/media/common/variant/flash-icon.svg',
+        imgheart:'https://img10.hkrtcdn.com/react/static/media/common/variant/wishlist/hover-heart.svg',
+        imgProduct:'./image/MuscleBlaze.jpg',
+        name:'MuscleBlaze Raw Whey Isolate,  2.2 lb  Unflavoured',
+        rating:4.7,
+        price:1899,
+        cartImg:"https://img2.hkrtcdn.com/react/static/media/common/new_cart.svg",
+        premiumPrice:1600
+         
+    },
+    {
+        imgflash: 'https://img10.hkrtcdn.com/react/static/media/common/variant/flash-icon.svg',
+        imgheart:'https://img10.hkrtcdn.com/react/static/media/common/variant/wishlist/hover-heart.svg',
+        imgProduct:'./image/Double-Rich-Chocolate.jpg',
+        name:'ON (Optimum Nutrition) Gold Standard 100% Whey Protein,  5 lb  Double Rich Chocolate ',
+        rating:4.3,
+        price:1899,
+        cartImg:"https://img2.hkrtcdn.com/react/static/media/common/new_cart.svg",
+        premiumPrice:1600,
+    },
+    {
+        imgflash: 'https://img10.hkrtcdn.com/react/static/media/common/variant/flash-icon.svg',
+        imgheart:'https://img10.hkrtcdn.com/react/static/media/common/variant/wishlist/hover-heart.svg',
+        imgProduct:'./image/Fruit-Splash-Watermelon.jpg',
+        name:'MuscleBlaze BCAA Pro,  2 Piece(s)/Pack  16 Servings  Fruit Splash & Watermelon ',
+        rating:3.7,
+        price:1899,
+        cartImg:"https://img2.hkrtcdn.com/react/static/media/common/new_cart.svg",
+        premiumPrice:1600
+    },
+    {
+        imgflash: 'https://img10.hkrtcdn.com/react/static/media/common/variant/flash-icon.svg',
+        imgheart:'https://img10.hkrtcdn.com/react/static/media/common/variant/wishlist/hover-heart.svg',
+        imgProduct:'./image/Peanut-Butter.jpg',
+        name:'MuscleBlaze Peanut Butter,  1 kg  Crunchy ',
+        rating:4.1,
+        price:1899,
+        cartImg:"https://img2.hkrtcdn.com/react/static/media/common/new_cart.svg",
+        premiumPrice:1600
+    },
+    {
+        imgflash: 'https://img10.hkrtcdn.com/react/static/media/common/variant/flash-icon.svg',
+        imgheart:'https://img10.hkrtcdn.com/react/static/media/common/variant/wishlist/hover-heart.svg',
+        imgProduct:'./image/whey.jpg',
+        name:'Ultimate Nutrition Prostar 100% Whey Protein,  5.28 lb  Chocolate Creme ',
+        rating:3.8,
+        price:1899,
+        cartImg:"https://img2.hkrtcdn.com/react/static/media/common/new_cart.svg",
+        premiumPrice:1600
+    },
+    {
+        imgflash: 'https://img10.hkrtcdn.com/react/static/media/common/variant/flash-icon.svg',
+        imgheart:'https://img10.hkrtcdn.com/react/static/media/common/variant/wishlist/hover-heart.svg',
+        imgProduct:'./image/bGreen.jpg',
+        name:'bGREEN Plant Protein by MuscleBlaze,  Chocolate  2.2 lb ',
+        rating:3.9,
+        price:1899,
+        cartImg:"https://img2.hkrtcdn.com/react/static/media/common/new_cart.svg",
+        premiumPrice:1600
+    },
+    {
+        imgflash: 'https://img10.hkrtcdn.com/react/static/media/common/variant/flash-icon.svg',
+        imgheart:'https://img10.hkrtcdn.com/react/static/media/common/variant/wishlist/hover-heart.svg',
+        imgProduct:'./image/Bold-Care-Surge-60-tablets.jpg',
+        name:'Bold Care Surge,  60 tablet(s) ',
+        rating:4.4,
+        price:1899,
+        cartImg:"https://img2.hkrtcdn.com/react/static/media/common/new_cart.svg",
+        premiumPrice:1600
+    },
+    
+    {
+        imgflash: 'https://img10.hkrtcdn.com/react/static/media/common/variant/flash-icon.svg',
+        imgheart:'https://img10.hkrtcdn.com/react/static/media/common/variant/wishlist/hover-heart.svg',
+        imgProduct:'./image/goldProtein.jpg',
+        name:'MuscleBlaze Whey Gold Protein,  4.4 lb  Rich Milk Chocolate ',
+        rating:4.5,
+        price:1899,
+        cartImg:"https://img2.hkrtcdn.com/react/static/media/common/new_cart.svg",
+        premiumPrice:1600,
+        trending:true
+         
+    },//
+    {
+        imgflash: 'https://img10.hkrtcdn.com/react/static/media/common/variant/flash-icon.svg',
+        imgheart:'https://img10.hkrtcdn.com/react/static/media/common/variant/wishlist/hover-heart.svg',
+        imgProduct:'./image/MuscleBlaze-Biozyme.jpg',
+        name:'MuscleBlaze Biozyme Whey Protein,  4.4 lb  Rich Milk Chocolate ',
+        rating:4.3,
+        price:1899,
+        cartImg:"https://img2.hkrtcdn.com/react/static/media/common/new_cart.svg",
+        premiumPrice:1600,
+        trending:true
+         
+    },
+    {
+        imgflash: 'https://img10.hkrtcdn.com/react/static/media/common/variant/flash-icon.svg',
+        imgheart:'https://img10.hkrtcdn.com/react/static/media/common/variant/wishlist/hover-heart.svg',
+        imgProduct:'./image/MuscleTech-NitroTech.jpg',
+        name:'MuscleTech NitroTech Performance Series,  4 lb  Milk Chocolate ',
+        rating:4.2,
+        price:1899,
+        cartImg:"https://img2.hkrtcdn.com/react/static/media/common/new_cart.svg",
+        premiumPrice:1600,
+        trending:true
+    },//
+    {
+        imgflash: 'https://img10.hkrtcdn.com/react/static/media/common/variant/flash-icon.svg',
+        imgheart:'https://img10.hkrtcdn.com/react/static/media/common/variant/wishlist/hover-heart.svg',
+        imgProduct:'./image/Myprotein-Impact.jpg',
+        name:'Myprotein Impact Whey Isolate,  1.1 lb  Chocolate Smooth ',
+        rating:4.1,
+        price:1899,
+        cartImg:"https://img2.hkrtcdn.com/react/static/media/common/new_cart.svg",
+        premiumPrice:1600,
+        trending:true
+    },
+    {
+        imgflash: 'https://img10.hkrtcdn.com/react/static/media/common/variant/flash-icon.svg',
+        imgheart:'https://img10.hkrtcdn.com/react/static/media/common/variant/wishlist/hover-heart.svg',
+        imgProduct:'./image/Optimum-Nutrition-Serious.jpg',
+        name:'ON (Optimum Nutrition) Serious Mass,  6 lb  Chocolate ',
+        rating:4.5,
+        price:1899,
+        cartImg:"https://img2.hkrtcdn.com/react/static/media/common/new_cart.svg",
+        premiumPrice:1600,
+        trending:true
+    },
+    ]
+let backgroundImages = [
+    {
+        image: './image/bnr_SBC1-top.jpg',
+        heading: "LEAN MUSCLE MASS",
+        topCategory: true 
+    },
+    {
+        image: './image/bnr_trueBasis.jpg',
+        heading: "Immunity & Well Being" ,
+        topCategory: true 
+    },
+    {
+        image: './image/bnr_Gold_SBC.jpg',
+        heading: "Bulk Gainer" ,
+        topCategory: true 
+    },
+    {
+        image: './image/bnr_trueBasisGold_SBC.jpg',
+        heading: "Stress & Relaxation" ,
+        topCategory: true 
+    },
+    {
+        image: './image/bnr_Slim_SBC.jpg',
+        heading: "Weight Loss" ,
+        topCategory: true 
+    },
+    {
+        image: './image/bnr_Xtend.jpg',
+        heading: "Muscle Recovery",
+        topCategory: true  
+    },
+    {
+        image: './image/bnr_healthPro_ESS.jpg',
+        heading: "Whey Proteins",
+        essentials: true  
+    },
+    {
+        image: './image/bnr_Gainer_ESS.jpg',
+        heading: "Gainers",
+        essentials: true  
+    },
+    {
+        image: './image/bnr_multivit_ESS.jpg',
+        heading: "Multivitamins",
+        essentials: true  
+    },
+    {
+        image: './image/bnr_omega3_ESS.jpg',
+        heading: "Omega 3",
+        essentials: true  
+    },
+    {
+        image: './image/bGreen.jpg',
+        heading: "plant protein",
+        essentials: true  
+    },
+    {
+        image: './image/bnr_bcca_ESS.jpg',
+        heading: "bcca",
+        essentials: true  
+    },
+]
+let productsWomen = [
+    {
+        imgProduct:'./image/bnr_multiVitamin.jpg',
+        name:'HealthKart HK Vitals Multivitamin with Multimineral, Amino Acids, Taurine & Ginseng Extract,  60 tablet(s)  Unflavoured ',
+        rating:4.5,
+        price:1899,
+    },
+    {
+        imgProduct:"./image/Vinegar.jpg",
+        name:'HealthKart Apple Cider Vinegar with Mother,  0.5 L  Ginger, Garlic, Lemon & Honey ',
+        rating:4.5,
+        price:1899,
+    },
+    {
+        imgProduct:"./image/bnr_haldi.jpg",
+        name:'HealthKart Easy Haldi,  100 g ',
+        rating:4.5,
+        price:1899,
+    },
+    {
+        imgProduct:'./image/bnr_lsopure.jpg',
+        name:'Isopure Low Carb,  1 lb  Dutch Chocolate ',
+        rating:4.5,
+        price:1899,
+    }
+    ]
+function appendProductForMen(homeWigCar,trend)
+{
+    productsMen.forEach(product => {
+        let container = document.createElement('div');
+        let contTip = document.createElement('div');
+        let flash = document.createElement('img'); 
+          flash.src = product.imgflash;
+         contTip.append(flash)
+         contTip.setAttribute('class','contTip') 
+    
+         let contMid= document.createElement('div');
+         contMid.setAttribute('class','contMid');
+         let imageHeartCont = document.createElement('div');
+         let imageHeart = document.createElement('img');
+         imageHeart.src = product.imgheart;
+         imageHeartCont.append(imageHeart);
+         imageHeartCont.setAttribute('class', 'logImg');
+    
+         let proImgCont= document.createElement('div');
+         let proImage = document.createElement('img');
+         proImage.src = product.imgProduct;
+         proImgCont.append(proImage);
+         proImgCont.setAttribute('class','middle')
+         
+         contMid.append(imageHeartCont,proImgCont);
+    
+         let bottomCont = document.createElement('div');
+         
+         let title = document.createElement('div');
+         title.textContent = product.name;
+         title.style.fontSize = '14px';
+         title.style.color = '#212121'
+         title.setAttribute('id','marg');
+          
+         let ratingCont = document.createElement('div');
+         let ratingInner = document.createElement('div');
+         ratingCont.setAttribute('class','rating');
+         ratingInner.textContent= "Rating: "+ product.rating
+         ratingCont.append(ratingInner)
+         ratingInner.setAttribute('class','innerRating')
+         let starPercentage = (product.rating/5)*100;
+         let roundedStars = Math.round(starPercentage/10)*10
+         console.log(starPercentage)
+       
+
+         let priceCont = document.createElement('div');
+         priceCont.setAttribute('id','priceCont')
+         let price = document.createElement('div');
+         price.setAttribute('class', 'price');
+         price.textContent = ' RS.'+ product.price + '/';
+    
+         var button = document.createElement('div');
+         button.setAttribute('class','addCart');
+         let cartIcon = document.createElement('img');
+         let span = document.createElement('span');
+         cartIcon.src = product.cartImg;
+         button.append(cartIcon)
+         span.textContent = "ADD"
+         button.appendChild(span)
+         priceCont.append(price,button)
+         console.log(product.cartImg)
+    
+         let hkPriceCont = document.createElement('div');
+        hkPriceCont.setAttribute('class','hk-premium-price');
+        let premPrice = document.createElement('div');
+        premPrice.textContent = 'Premium Price: '+ product.premiumPrice;
+        hkPriceCont.append(premPrice)
+        
+        bottomCont.append(title,ratingCont,priceCont,hkPriceCont);
+       
+
+        container.append(contTip,contMid,bottomCont);
+      console.log(trend);
+        if(product['trending'] === true)
+        {
+            trend.appendChild(container)
+        }
+        else{
+            homeWigCar.appendChild(container);
+        }
+        container.style.cursor = 'pointer'
+        container.onmouseenter = function(){
+            button.style.backgroundColor = 'rgb(0,194,193)'
+        }
+        container.onmouseleave = function(){
+            button.style.backgroundColor = 'rgb(225,237,244)'
+        }
+    });
+}
+
+function appendBackground(topCategory,type)
+{
+    backgroundImages.forEach(element=>{
+
+            let divElement = document.createElement('div')
+            let title = document.createElement('h3')
+            let horLine = document.createElement('div')
+            divElement.style.backgroundImage = `URL('${element.image}')`;
+            title.textContent = element.heading;
+            title.setAttribute('class' ,'backHeadings')
+            title.appendChild(horLine);
+            title.style.textTransform = 'uppercase'
+            horLine.setAttribute('class','horLine')
+           divElement.appendChild(title);
+
+         if(element.topCategory == true && type=='top')
+         { 
+            topCategory.appendChild(divElement)  
+         }else if(element.essentials == true && type=='ess'){
+            topCategory.appendChild(divElement)
+            title.style.color ='gray'
+         }   
+    })
+}
+
+function appendProductForWomen(prodDesc){
+
+    productsWomen.forEach(product =>{
+        let container = document.createElement('div');
+        let image = document.createElement('div');
+        let proImg = document.createElement('img');
+        proImg.src = product.imgProduct;
+         image.setAttribute('class','imageOfPro');
+         image.appendChild(proImg);
+
+         let title = document.createElement('div');
+         let reviewStars = document.createElement('div');
+         let prise = document.createElement('div');
+         title.textContent = product.name;
+         title.setAttribute('class',"title")
+
+         reviewStars.textContent = "Rating: " +product.rating;
+         reviewStars.setAttribute('class', 'reviewStars')
+
+         prise.textContent = 'Rs. '+ product.price;
+         prise.setAttribute('class','price')
+
+         container.append(image,title,reviewStars,prise)
+
+         prodDesc.appendChild(container)
+     })   
+}
+
+function addScripts(){
+
+    var homeWigCar = document.getElementById('home-widgit-carousel');
+  var trend = document.getElementById("trending");
+  var topCat = document.getElementById("top-Categories");
+  var essential = document.getElementById("essentials");
+  let tc = 'top', ess = 'ess';
+  appendProductForMen(homeWigCar,trend)
+  appendBackground(topCat,tc)
+  appendBackground(essential,ess)
+
+var i =0;
+  let slide = document.getElementById('slider')
+   let btn1 = document.getElementById('btn1')
+   let btn2 = document.getElementById('btn2')
+   let btn3 = document.getElementById('btn3')
+   let btn4 = document.getElementById('btn4')
+
+   let interval =setInterval(translate, 4000)
+   let arr = ["0px","-100%","-200%","-300%"]
+   function translate(){
+       if(i==4)
+       {
+           clearInterval(interval)
+       }
+      slide.style.transform = `translateX(${arr[i]})`
+      i++;
+   }
+     btn1.onclick =() =>{
+         slide.style.transform = 'translateX(0px)'
+         btn1.classList.add('active')
+         btn2.classList.remove('active')
+         btn3.classList.remove('active')
+         btn4.classList.remove('active')
+     }
+     btn2.onclick =() =>{
+         slide.style.transform = 'translateX(-100%)'
+         btn2.classList.add('active')
+         btn1.classList.remove('active')
+         btn3.classList.remove('active')
+         btn4.classList.remove('active')
+     }
+     btn3.onclick =() =>{
+         slide.style.transform = 'translateX(-200%)'
+         btn3.classList.add('active')
+         btn2.classList.remove('active')
+         btn1.classList.remove('active')
+         btn4.classList.remove('active')
+     }
+     btn4.onclick =() =>{
+         slide.style.transform = 'translateX(-300%)'
+         btn4.classList.add('active')
+         btn2.classList.remove('active')
+         btn3.classList.remove('active')
+         btn1.classList.remove('active')
+     }
+}
+export {appendProductForMen ,appendBackground ,appendProductForWomen,addScripts}
---
+++ scripts/navbar.js
@@ -0,0 +1,81 @@
+function onMouseIn(hCons, myDIV){
+    hCons.onmouseover = ()=> 
+    {
+        addDropDwn(myDIV)
+    }
+}
+function onMouserOut(hCons, myDIV){
+   hCons.onmouseleave = ()=> 
+    {
+        removeDropDwn(myDIV)
+    }
+}
+function addDropDwn(x) {
+      
+    if (x.style.display === "none") {
+      x.style.display = "grid";
+      x.style.position ='absolute'
+    }
+  }
+  function removeDropDwn(x){
+      x.style.display = "none";
+     }
+
+ function addDropDwn1(mO,x ,array) {
+        mO.onmouseover =()=> {
+            array.forEach((ele)=>{
+                 ele.style.display = "none"
+          });
+        if (x.style.display === "none") {
+          x.style.display = "grid";
+          x.style.position ='absolute';
+          x.style.backgroundColor = 'white'
+        }
+       }
+      }
+      function getElement(id)
+      {
+          return document.getElementById(id);
+      }
+    
+        function makeArr(array){
+            let arr = []
+            for(let i=0; i< array.length; i++)
+            {
+                arr.push(document.getElementById(array[i]))
+            }
+            return arr;
+        }
+        function makeNavbar(){
+            let hCons = getElement('hk');
+            let myDIV = getElement('myDIV');
+            onMouseIn(hCons,myDIV)
+            onMouserOut(hCons,myDIV)
+        
+          let bestSeller = getElement('bestSells');
+          let catBestSell = getElement('catogBestSeller')
+           onMouseIn(bestSeller,catBestSell)
+           onMouserOut(bestSeller, catBestSell)
+          let sNutri = getElement('sNutri')
+          let hNutri = getElement('hNutri')
+          let ar = makeArr(['forSport','forhealth'])
+          addDropDwn1(sNutri,ar[0],ar)
+          addDropDwn1(hNutri,ar[1],ar)
+          
+          let accounts = getElement('accounts')
+          let dT = getElement('dropTable')
+          onMouseIn(accounts,dT)
+          onMouserOut(accounts,dT)
+
+          let genderSec = document.getElementById('genderSec');
+  genderSec.onmouseover = ()=>{
+       let gendDroDwn = document.getElementById('gend-DroDwn')
+       gendDroDwn.style.display = 'grid'
+  }
+  genderSec.onmouseleave = ()=>{
+       let gendDroDwn = document.getElementById('gend-DroDwn')
+       gendDroDwn.style.display = 'none'
+  }
+  
+         }
+export {makeNavbar}
