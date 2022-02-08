069c1c55ec302a6ff0e65004ae929a89d8262f49
Hemant Kumar Jayas <87421907+hemantjayas@users.noreply.github.com>
2022-02-08 20:09:15 +0530

    s

--- cartcount.js
+++
@@ -1,9 +0,0 @@
-
-let txt = document.getElementById('cartOnNum')
-let cart = JSON.parse(localStorage.getItem('cart'))
-
-let spantext = document.createElement('span')
-var len = cart.length;
-spantext.innerHTML = len;
-
-txt.appendChild(spantext)
